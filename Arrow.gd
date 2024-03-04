5964a8ea09161179e9cd7f8781c6951751032340
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2024-03-04 21:29:30 +0100

    Allow changing  the arrow texture

--- Arrow.gd
+++ Arrow.gd
@@ -5,14 +5,35 @@ const SPEED = 600.0
 var direction = Vector2()
 var area = Area2D.new()
 # Get the gravity from the project settings to be synced with RigidBody nodes
-const row = 15
+@export var arrow_texture = ArrowTexture.STEEL
+
+enum ArrowTexture {
+	STEEL,
+	WOOD,
+	RED,
+	BLUE,
+	PINK,
+	GREEN,
+	DART,
+}
+
+func texture_to_column_row(texture):
+	match texture:
+		ArrowTexture.STEEL: return Vector2(0, 15)
+		ArrowTexture.WOOD: return Vector2(8, 15)
+		ArrowTexture.RED: return Vector2(0, 13)
+		ArrowTexture.BLUE: return Vector2(0, 14)
+		ArrowTexture.PINK: return Vector2(8, 14)
+		ArrowTexture.GREEN: return Vector2(8, 13)
+		ArrowTexture.DART: return Vector2(6, 5)
+		_: return Vector2(0, 15)
 
 func set_direction(new_direction):
 	direction = new_direction
 
-func direction_to_column(direction):
+func direction_to_column(in_direction):
 	# down = 0, down left = 1, left = 2, up left = 3, up = 4, up right = 5, right = 6, down right = 7
-	match direction:
+	match in_direction:
 		Vector2(0, 1): return 0
 		Vector2(-1, 1): return 1
 		Vector2(-1, 0): return 2
@@ -30,7 +51,9 @@ func _ready():
 	var atlas_texture = ImageTexture.create_from_image(atlas_image)
 	atlas.atlas = atlas_texture
 	var column = direction_to_column(direction)
-	var region = Rect2(Vector2(column * 8, row * 8), Vector2(8, 8))
+	var type = texture_to_column_row(arrow_texture)
+	type.x += column
+	var region = Rect2(type * 8, Vector2(8, 8))
 	atlas.region = region
 	var sprite = Sprite2D.new()
 	sprite.texture = atlas
